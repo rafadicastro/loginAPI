eb0f3753e0b6bf285e7c24ee73761fc7c273cef4
Rafael Sousa <sousarafael151@gmail.com>
2024-10-19 11:47:07 -0300

    Alterando repositório

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('./config/db')
 
 const express = require('express');
 const app = express();
-const port = 1310
+const port = process.env.PORT || 1310;
 
 const userRouter = require('./api/User')
 
